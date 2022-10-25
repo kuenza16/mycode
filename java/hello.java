412431702ab013d50254cf2db21fd289dc7a00c8
Kuenzang <12210063.gcit@rub.edu.bt>
2022-10-25 23:28:41 +0600

    multilevel inheritence

--- java/hello.java
+++ java/hello.java
@@ -1,5 +1,15 @@
-public class hello{
-    public static void main(String[] args) {
-        System.out.println("Helloo");
+class Vehicle1{
+    void run3(){System.out.println("vehicle is the parent class");}
     }
-}
+    class Bike1 extends Vehicle1{
+    void run2(){System.out.println("Bike has some properties of a vehicle");}
+    }
+    class Cycle extends Bike1{
+    void run1(){System.out.println("Cycle has some properties of Bike");}
+    }
+    public class hello{
+    public static void main(String[] args){
+    Cycle d = new Cycle();
+    d.run1();
+    d.run2(); 
+    d.run3(); } }
